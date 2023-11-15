9af8ea45cebbaabef4b0d1e33ebe7a2bd4084c72
yucolax <yucolax@gmail.com>
2023-11-15 01:59:16 +0000

    WIP

--- whisky/02-script-analysis-whisky.R
+++ whisky/02-script-analysis-whisky.R
@@ -45,6 +45,9 @@ tabla_lealtad %>%
   # renombrando una columna
   rename(Marca = `¿Cuál es la marca que más compra?`) %>% 
   
+  # reordenando los niveles de Marca
+  mutate(Marca = fct_reorder(Marca, n, .desc = T)) %>% 
+  
   ggplot(aes(x = Marca,
              y = proporción,
              fill = Marca,
@@ -57,7 +60,7 @@ tabla_lealtad %>%
        subtitle = "¿Cuál es la marca que más compra?",
        caption = "Johnnie Walker es la marca con mayor índice de lealtad") +
   
-  scale_y_continuous(labels = scales::percent) +
+  # scale_y_continuous(labels = scales::percent) +
   scale_fill_viridis_d() +
   theme_minimal() +
   theme(legend.position = "none",
@@ -67,22 +70,147 @@ tabla_lealtad %>%
 
 
 
+## Prueba de marca
+
+DF3 %>% 
+  pivot_longer(cols = starts_with("Prueba"),
+               names_to = "Variable",
+               values_to = "Marca") %>%
+  select(Marca) %>% 
+  na.omit() %>% 
+  count(Marca) %>% 
+  mutate(proporción = n/nrow(DF3),
+         porcentaje = scales::percent(proporción),
+         Marca = fct_reorder(Marca, n, .desc = T)) %>% 
+  
+  ggplot(aes(x = Marca,
+             y = proporción,
+             fill = Marca,
+             label = porcentaje)) +
+  
+  geom_col() +
+  geom_label(fill = "white") +
+  
+  labs(title = "Niveles de Prueba",
+       subtitle = "¿Cuáles de las siguientes marcas ha probado?") +
+  
+  scale_fill_viridis_d() +
+  theme_minimal() +
+  theme(legend.position = "none",
+        axis.title = element_blank(),
+        axis.text.y = element_blank(),
+        panel.grid = element_blank())
 
 
 
+# Función que crea la tabla
+
+tabla_multiple <- function(dataFrame, indicador) {
+  
+  dataFrame %>% 
+    pivot_longer(cols = starts_with(indicador),
+                 names_to = "Variable",
+                 values_to = "Marca") %>% 
+    select(Marca) %>% 
+    na.omit() %>% 
+    count(Marca) %>% 
+    mutate(proporción = n/nrow(dataFrame),
+           porcentaje = scales::percent(proporción),
+           KPI = rep(indicador, times = nrow(.)))
+  
+}
+
+tabla_conocimiento <- tabla_multiple(dataFrame = DF3,
+                                     indicador = "Conocimiento")
+
+tabla_prueba <- tabla_multiple(dataFrame = DF3,
+                               indicador = "Prueba")
+
+tabla_lealtad <- tabla_lealtad %>% 
+  rename(Marca = `¿Cuál es la marca que más compra?`) %>% 
+  mutate(KPI = rep("Lealtad", times = nrow(.)))
 
 
 
+# Tabla de consideración
+
+tabla_consideración <- DF3 %>% 
+  select(starts_with("Consideración")) %>% 
+  pivot_longer(cols = everything(),
+               names_to = "Marca") %>% 
+  mutate(t2b = ifelse(test = value %in% c("Creo que SÍ la consideraría",
+                                          "Sería mi 1ra opción"),
+                      yes = 1,
+                      no = 0),
+         Marca = case_when(
+           Marca == "Consideración Chivas" ~ "Chivas Regal",
+           Marca == "Consideración Johnny Walker" ~ "Johnnie Walker",
+           Marca == "Consideración Old Parr" ~ "Old Parr",
+           Marca == "Consideración Jack Daniel's" ~ "Jack Daniel's",
+           .default = NA
+         )) %>%
+  group_by(Marca) %>% 
+  summarise(n = sum(t2b)) %>% 
+  ungroup() %>% 
+  mutate(proporción = n/nrow(DF3),
+         porcentaje = scales::percent(proporción),
+         KPI = rep("Consideración", times = nrow(.)))
 
 
 
+# Tabla de recomendación
+
+tabla_recomendación <- DF3 %>% 
+  select(starts_with("Recomendación")) %>% 
+  pivot_longer(cols = everything(),
+               names_to = "Marca") %>% 
+  mutate(t2b = ifelse(test = value %in% c(9:10),
+                      yes = 1,
+                      no = 0),
+         Marca = case_when(
+           Marca == "Recomendación Chivas" ~ "Chivas Regal",
+           Marca == "Recomendación Johnny Walker" ~ "Johnnie Walker",
+           Marca == "Recomendación Old Parr" ~ "Old Parr",
+           Marca == "Recomendación Jack Daniel's" ~ "Jack Daniel's",
+           .default = NA
+         )) %>%
+  group_by(Marca) %>% 
+  summarise(n = sum(t2b)) %>% 
+  ungroup() %>% 
+  mutate(proporción = n/nrow(DF3),
+         porcentaje = scales::percent(proporción),
+         KPI = rep("Recomendación", times = nrow(.)))
 
 
 
+# Tabla concatenada
 
+tabla_concat <- bind_rows(tabla_conocimiento,
+                          tabla_consideración,
+                          tabla_prueba,
+                          tabla_lealtad,
+                          tabla_recomendación) %>% 
+  
+  mutate(KPI = fct_relevel(KPI, c("Conocimiento",
+                                  "Consideración",
+                                  "Prueba",
+                                  "Lealtad",
+                                  "Recomendación")))
 
-
-
+tabla_concat %>%
+  ggplot(aes(x = KPI,
+             y = proporción,
+             fill = KPI,
+             label = porcentaje)) +
+  geom_col() +
+  geom_label(fill = "white") +
+  facet_wrap(~ Marca, nrow = 2) +
+  scale_y_continuous(labels = scales::percent,
+                     limits = c(0,1)) +
+  theme_minimal() +
+  theme(legend.position = "none",
+        axis.title = element_blank(),
+        panel.grid = element_blank())
 
 
 
